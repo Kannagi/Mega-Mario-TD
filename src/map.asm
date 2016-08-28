85e8837419263ca69467d4b243ca292f9a0d1b2a
Kannagi <kannagichan@gmail.com>
2016-08-28 18:11:03 +0200

    Init commit

---
+++ src/map.asm
@@ -0,0 +1,386 @@
+
+    dc.l $00000000
+    dc.l $00000005
+    dc.l $00000056
+    dc.l $00005555
+    dc.l $00055675
+    dc.l $00058667
+    dc.l $00588667
+    dc.l $00568667
+
+    dc.l $00566667
+    dc.l $05555767
+    dc.l $55675577
+    dc.l $56867575
+    dc.l $56867556
+    dc.l $56667968
+    dc.l $55677566
+    dc.l $05555555
+
+    dc.l $00000000
+    dc.l $55550000
+    dc.l $66755000
+    dc.l $68675500
+    dc.l $56867500
+    dc.l $55667500
+    dc.l $75677500
+    dc.l $75655500
+
+    dc.l $75677550
+    dc.l $56887755
+    dc.l $56686775
+    dc.l $55566775
+    dc.l $67756779
+    dc.l $86777779
+    dc.l $66777799
+    dc.l $55555550
+
+    dc.l $12312312
+    dc.l $23123122
+    dc.l $21113222
+    dc.l $11322213
+    dc.l $13321232
+    dc.l $22222211
+    dc.l $32211113
+    dc.l $12231213
+
+    dc.l $22112332
+    dc.l $23123122
+    dc.l $21113222
+    dc.l $11322213
+    dc.l $13321232
+    dc.l $22222211
+    dc.l $32211113
+    dc.l $12231213
+
+    dc.l $23112312
+    dc.l $23123122
+    dc.l $21113222
+    dc.l $11322213
+    dc.l $13321232
+    dc.l $22222211
+    dc.l $32211113
+    dc.l $12231213
+
+    dc.l $22112332
+    dc.l $23123122
+    dc.l $21113222
+    dc.l $11322213
+    dc.l $13321232
+    dc.l $22222211
+    dc.l $32211113
+    dc.l $12231213
+
+    dc.l $12312312
+    dc.l $23123122
+    dc.l $21113222
+    dc.l $11322213
+    dc.l $11111111
+    dc.l $11111111
+    dc.l $44444444
+    dc.l $44444444
+
+    dc.l $44444444
+    dc.l $44444444
+    dc.l $11111111
+    dc.l $11111111
+    dc.l $13321232
+    dc.l $22221211
+    dc.l $32211113
+    dc.l $12231213
+
+    dc.l $23112312
+    dc.l $23123122
+    dc.l $21113222
+    dc.l $11322213
+    dc.l $11111111
+    dc.l $11111111
+    dc.l $44444444
+    dc.l $44444444
+
+    dc.l $44444444
+    dc.l $44444444
+    dc.l $11111111
+    dc.l $11111111
+    dc.l $13321232
+    dc.l $22222211
+    dc.l $32211113
+    dc.l $12231213
+
+    dc.l $12311144
+    dc.l $23121144
+    dc.l $21111144
+    dc.l $11321144
+    dc.l $13321144
+    dc.l $22221144
+    dc.l $32211144
+    dc.l $12231144
+
+    dc.l $22111144
+    dc.l $23121144
+    dc.l $21111144
+    dc.l $11321144
+    dc.l $13321144
+    dc.l $22221144
+    dc.l $32211144
+    dc.l $12231144
+
+    dc.l $44112312
+    dc.l $44113122
+    dc.l $44113222
+    dc.l $44112213
+    dc.l $44111232
+    dc.l $44112211
+    dc.l $44111113
+    dc.l $44111213
+
+    dc.l $44112332
+    dc.l $44113122
+    dc.l $44113222
+    dc.l $44112213
+    dc.l $44111232
+    dc.l $44112211
+    dc.l $44111113
+    dc.l $44111213
+
+    dc.l $12312312
+    dc.l $23123122
+    dc.l $21113222
+    dc.l $11322213
+    dc.l $11111111
+    dc.l $11111111
+    dc.l $44444444
+    dc.l $44444444
+
+    dc.l $44444444
+    dc.l $44444444
+    dc.l $11111144
+    dc.l $11111144
+    dc.l $13321144
+    dc.l $22221144
+    dc.l $32211144
+    dc.l $12231144
+
+    dc.l $23112312
+    dc.l $23123122
+    dc.l $21113222
+    dc.l $11322213
+    dc.l $11121232
+    dc.l $11112211
+    dc.l $44111113
+    dc.l $44111213
+
+    dc.l $44112332
+    dc.l $44113122
+    dc.l $44113222
+    dc.l $44112213
+    dc.l $44111232
+    dc.l $44112211
+    dc.l $44111113
+    dc.l $44111213
+
+    dc.l $12312312
+    dc.l $23123122
+    dc.l $21113222
+    dc.l $11322213
+    dc.l $13322111
+    dc.l $22221111
+    dc.l $32211144
+    dc.l $12231144
+
+    dc.l $22111144
+    dc.l $23121144
+    dc.l $21111144
+    dc.l $11321144
+    dc.l $13321144
+    dc.l $22221144
+    dc.l $32211144
+    dc.l $12231144
+
+    dc.l $23112312
+    dc.l $23123122
+    dc.l $21113222
+    dc.l $11322213
+    dc.l $11111111
+    dc.l $11111111
+    dc.l $44444444
+    dc.l $44444444
+
+    dc.l $44444444
+    dc.l $44444444
+    dc.l $44111111
+    dc.l $44111111
+    dc.l $44111232
+    dc.l $44112211
+    dc.l $44111113
+    dc.l $44111213
+
+    dc.l $12311144
+    dc.l $23121144
+    dc.l $21111144
+    dc.l $11321144
+    dc.l $13321144
+    dc.l $22221144
+    dc.l $32211144
+    dc.l $12231144
+
+    dc.l $22111144
+    dc.l $23121144
+    dc.l $21111111
+    dc.l $11322111
+    dc.l $13321232
+    dc.l $22222211
+    dc.l $32211113
+    dc.l $12231213
+
+    dc.l $44112312
+    dc.l $44113122
+    dc.l $44113222
+    dc.l $44112213
+    dc.l $44111232
+    dc.l $44111111
+    dc.l $44111111
+    dc.l $44444444
+
+    dc.l $44444444
+    dc.l $44444444
+    dc.l $11111111
+    dc.l $11111111
+    dc.l $13321232
+    dc.l $22222211
+    dc.l $32211113
+    dc.l $12231213
+
+    dc.l $12311144
+    dc.l $23121144
+    dc.l $21111144
+    dc.l $11321144
+    dc.l $13321144
+    dc.l $11111144
+    dc.l $11111144
+    dc.l $44444444
+
+    dc.l $44444444
+    dc.l $44444444
+    dc.l $11111111
+    dc.l $11111111
+    dc.l $13321232
+    dc.l $22222211
+    dc.l $32211113
+    dc.l $12231213
+
+    dc.l $44112312
+    dc.l $44113122
+    dc.l $44113222
+    dc.l $44112213
+    dc.l $44111232
+    dc.l $44112211
+    dc.l $44111113
+    dc.l $44111213
+
+    dc.l $44112332
+    dc.l $44113122
+    dc.l $11113222
+    dc.l $11122213
+    dc.l $13321232
+    dc.l $22222211
+    dc.l $32211113
+    dc.l $12231213
+
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+    dc.l $cccaaaaa
+    dc.l $cacccaaa
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaf
+    dc.l $aaaaaccc
+    dc.l $aaaaaaaa
+
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+    dc.l $aacccaaa
+    dc.l $cccaccca
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+    dc.l $aaaaaacc
+    dc.l $aaaaaaaa
+    dc.l $ffaaaaaa
+    dc.l $acccaaaa
+    dc.l $aaaaaaaa
+
+    dc.l $aaaaaaaa
+    dc.l $aafffaaa
+    dc.l $cccaccca
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+    dc.l $aaaaaaaa
+
+    dc.l $99999999
+    dc.l $9bbbbbbb
+    dc.l $bddddddd
+    dc.l $bdeeeeee
+    dc.l $555555e5
+    dc.l $5d5e5e5e
+    dc.l $5dee5e5e
+    dc.l $555e5e55
+
+    dc.l $bd5e5e5e
+    dc.l $5d5e5e5e
+    dc.l $555e5e5e
+    dc.l $bdeeeeee
+    dc.l $bdeeeeee
+    dc.l $bddddddd
+    dc.l $5bbbbbbb
+    dc.l $55555555
+
+    dc.l $99999955
+    dc.l $bbbbbbb5
+    dc.l $dddddddb
+    dc.l $eeeeeedb
+    dc.l $ee55e555
+    dc.l $5e5e5e5b
+    dc.l $5e5e5e5b
+    dc.l $5e55ee5b
+
+    dc.l $5e5e5e5b
+    dc.l $5e5e5e5b
+    dc.l $5e5e5e5b
+    dc.l $eeeeeedb
+    dc.l $eeeeeedb
+    dc.l $dddddddb
+    dc.l $bbbbbbb5
+    dc.l $55555555
+
+
+
+palette_map:
+    dc.w $0e0e
+    dc.w $08e6
+    dc.w $0682
+    dc.w $06c2
+    dc.w $08ec
+    dc.w $0000
+    dc.w $026c
+    dc.w $0228
+    dc.w $068c
+    dc.w $0000
+    dc.w $0e86
+    dc.w $000c
+    dc.w $0ec8
+    dc.w $048e
+    dc.w $06ce
+    dc.w $0eee
+
+
+
+
+;palette size octet : 32 ,hexa $0020
+;size octet : 2496 ,hexa $09c0,tile : 39
